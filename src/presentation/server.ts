07db8ffd4d5fe688c6bdf4b6bf3359cc1b1ae61d
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2025-12-30 20:20:19 -0500

    feat: add cron service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,10 @@
+import { CronService } from "./cron/cron-service";
+
 export class Server {
   public static start() {
     console.log("Server started...");
+    CronService.createJob("*/5 * * * * *", () => {
+      console.log("executed every 2 seconds");
+    }).start();
   }
 }
