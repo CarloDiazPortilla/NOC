d2a6e0b163507d0658039c48bfb24c98acd9ef31
Carlo Rodrigo Diaz Portilla <cdiazportillap@gmail.com>
2026-01-12 13:02:15 -0500

    feat: implement saveLog

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -5,7 +5,7 @@ import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 export class FileSystemDatasource implements LogDatasource {
 
   private readonly logPath = "logs/";
-  private readonly allLogsPath = "logs/logs-low.log";
+  private readonly allLogsPath = "logs/logs-all.log";
   private readonly midSeverityLogsPath = "logs/logs-medium.log";
   private readonly highSeverityLogsPath = "logs/logs-high.log";
 
@@ -24,13 +24,22 @@ export class FileSystemDatasource implements LogDatasource {
     if (!fs.existsSync(path)) fs.writeFileSync(path, "");
   }
 
-  saveLog(log: LogEntity): Promise<void> {
+  async saveLog(log: LogEntity): Promise<void> {
+    const logMessage = `${JSON.stringify(log)}\n`;
+    fs.appendFileSync(this.allLogsPath, logMessage);
+    switch (log.severityLevel) {
+      case LogSeverityLevel.MEDIUM:
+        fs.appendFileSync(this.midSeverityLogsPath, logMessage);
+        break;
+      case LogSeverityLevel.HIGH:
+        fs.appendFileSync(this.highSeverityLogsPath, logMessage);
+        break;
+    }
+  }
+  async getLogs(): Promise<LogEntity[]> {
     throw new Error("Method not implemented.");
   }
-  getLogs(): Promise<LogEntity[]> {
-    throw new Error("Method not implemented.");
-  }
-  getLogsBySeverity(severity: LogSeverityLevel): Promise<LogEntity[]> {
+  async getLogsBySeverity(severity: LogSeverityLevel): Promise<LogEntity[]> {
     throw new Error("Method not implemented.");
   }
 
