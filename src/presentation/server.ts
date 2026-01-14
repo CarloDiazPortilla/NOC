185d351e5639e45459ca4d7718079075e9331dcc
Carlo Rodrigo Diaz Portilla <cdiazportillap@gmail.com>
2026-01-14 13:16:24 -0500

    fix: add fileSystemLogRepository to CronService

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,14 +1,19 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log-impl.repository";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
 
 export class Server {
   public static start() {
     console.log("Server started...");
     CronService.createJob("*/5 * * * * *", () => {
+      const url = "http://localhost:3000";
+      // const url = "https://google.com";
       new CheckService(
-        () => console.log("Success"),
-        (error) => console.log(error)
-      ).execute("https://google.com");
+        fileSystemLogRepository,
+      ).execute(url);
     });
   }
 }
