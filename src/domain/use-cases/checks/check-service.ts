c5a071d377066171c55782d18080aebe6456b01c
Carlo Rodrigo Diaz Portilla <cdiazportillap@gmail.com>
2026-01-14 13:14:26 -0500

    fix: make success and error callback optional

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -5,16 +5,16 @@ export interface CheckServiceUseCase {
   execute: (url: string) => Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 
 export class CheckService implements CheckServiceUseCase {
 
   constructor(
     private readonly logRepository: LogRepository,
-    private readonly successCallback: SuccessCallback,
-    private readonly errorCallback: ErrorCallback
+    private readonly successCallback: SuccessCallback = undefined,
+    private readonly errorCallback: ErrorCallback = undefined
   ) {
 
   }
@@ -27,13 +27,13 @@ export class CheckService implements CheckServiceUseCase {
       }
       const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.LOW);
       this.logRepository.saveLog(log);
-      this.successCallback();
+      this.successCallback && this.successCallback();
       return true;
     } catch (error) {
       const errorMessage = `${error}`
-      const log = new LogEntity(errorMessage, LogSeverityLevel.HIGH);
+      const log = new LogEntity(`Service ${url} not working with error: ${errorMessage}`, LogSeverityLevel.HIGH);
       this.logRepository.saveLog(log);
-      this.errorCallback(errorMessage);
+      this.errorCallback && this.errorCallback(errorMessage);
       return false;
     }
   }
